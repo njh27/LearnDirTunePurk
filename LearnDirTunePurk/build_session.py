51d3d13a794cc8b8dcee90458b3088551026f13f
njh27 <njh27@duke.edu>
2022-03-08 12:39:50 -0500

    started function for determining learning pursuit vectors

--- LearnDirTunePurk/build_session.py
+++ LearnDirTunePurk/build_session.py
@@ -30,10 +30,6 @@ def create_behavior_session(maestro_dir, session_name=None, check_existing=True,
     sess = sa.session.Session(trial_list, session_name=session_name)
     sess.add_trial_data(trial_list_bhv, data_type=None)
 
-    # Align target onset with monitor refresh rate and align
-    sess.shift_event_to_refresh('target_onset')
-    sess.align_trial_data('target_onset', alignment_offset=0)
-
     # Add hard coded blocks by trial names
     trial_names = ['d014fix', 'd0-14fix', 'd-1010fix', 'd1010fix', 'd140fix', 'd-10-10fix', 'd10-10fix', 'd-140fix', 'd00fix']
     block_names = ['fix_tune1', 'fix_tune2']
@@ -99,5 +95,42 @@ def create_behavior_session(maestro_dir, session_name=None, check_existing=True,
     sess.add_blocks(trial_names, block_names, ignore_trial_names=ignore_trial_names,
                     max_consec_absent=10, block_min=20)
 
+    # Align target onset with monitor refresh rate and align
+    sess.shift_event_to_refresh('target_onset')
+    sess.align_trial_data('target_onset', alignment_offset=0)
+    sess.align_trial_data('fixation_onset', alignment_offset=0, trials='fix_tune1')
+    sess.align_trial_data('fixation_onset', alignment_offset=0, trials='fix_tune2')
+
 
     return sess
+
+
+def parse_learning_directions(sess):
+    """ Parse through the block/trial names established above to find the main
+    learning block and direction. This then allows us to determine the other
+    directions and unlearning with respect to this etc. """
+    n_max_learn = 0
+    b_max_learn = None
+    for block in sess.blocks.keys():
+        if "Learn" in block:
+            block_len = sess.blocks[block][1] - sess.blocks[block][0]
+            if block_len > n_max_learn:
+                n_max_learn = block_len
+                b_max_learn = block
+    if b_max_learn is None:
+        # We did NOT find a learning block
+        return
+    # We found a learning block so carry on
+    pursuit_dir, learn_dir = b_max_learn.split("Learn")
+    pursuit_dir = int(pursuit_dir)
+    learn_dir = int(learn_dir)
+    anti_pursuit_dir = (pursuit_dir + 180) % 360
+    anti_learn_dir = (learn_dir + 180) % 360
+
+    # Rotation matrix so pursuit direction is at 0 degrees
+    rotate_pursuit_mat = np.array([[np.cos(np.deg2rad(0 - pursuit_dir)), -1*np.sin(np.deg2rad(0 - pursuit_dir))],
+                                   [np.sin(np.deg2rad(0 - pursuit_dir)), np.cos(np.deg2rad(0 - pursuit_dir))]])
+    # Unit vector in learing direction (round to 3 decimals to remove precision error from trig above
+    learn_vector = np.around(np.array([np.cos(np.deg2rad(learn_dir)), np.sin(np.deg2rad(learn_dir))]), 3)
+    # Transformation of learning vector with rotation matrix and mirror if needed
+    rotate_pursuit_mat = rotate_pursuit_mat @ np.array([[1., 0.], rotate_pursuit_mat @ learn_vector])
