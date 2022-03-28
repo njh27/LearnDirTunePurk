e0b1ec5ab4b19811ea3c22f0a2690d4ca9c9bb39
njh27 <njh27@duke.edu>
2022-03-28 12:48:19 -0400

    made ldp session object derived from session class

--- LearnDirTunePurk/build_session.py
+++ LearnDirTunePurk/build_session.py
@@ -1,6 +1,7 @@
 import numpy as np
 from LearnDirTunePurk.load_data import load_maestro_directory
 from LearnDirTunePurk import format_trials
+from LearnDirTunePurk.ldp_session import LDPSession
 import ReadMaestro as rm
 import SessionAnalysis as sa
 
@@ -27,12 +28,13 @@ def create_behavior_session(maestro_dir, session_name=None, check_existing=True,
                         maestro_data, 1, start_data=0, data_name="eye")
 
     # Create base session from apparatus trials then add behavior
-    sess = sa.session.Session(trial_list, session_name=session_name)
+    # sess = sa.session.Session(trial_list, session_name=session_name)
+    sess = LDPSession(trial_list, session_name=session_name)
     sess.add_trial_data(trial_list_bhv, data_type=None)
 
     # Add hard coded blocks by trial names
     trial_names = ['d014fix', 'd0-14fix', 'd-1010fix', 'd1010fix', 'd140fix', 'd-10-10fix', 'd10-10fix', 'd-140fix', 'd00fix']
-    block_names = ['fix_tune1', 'fix_tune2']
+    block_names = ['FixTunePre', 'FixTunePost']
     sess.add_blocks(trial_names, block_names, block_min=9)
 
     trial_names = ['270RandVP', '90RandVP', '180RandVP', '0RandVP']
@@ -98,82 +100,9 @@ def create_behavior_session(maestro_dir, session_name=None, check_existing=True,
     # Align target onset with monitor refresh rate and align
     sess.shift_event_to_refresh('target_onset')
     sess.align_trial_data('target_onset', alignment_offset=0)
-    sess.align_trial_data('fixation_onset', alignment_offset=0, trials='fix_tune1')
-    sess.align_trial_data('fixation_onset', alignment_offset=0, trials='fix_tune2')
+    sess.align_trial_data('fixation_onset', alignment_offset=0, trials='FixTunePre')
+    sess.align_trial_data('fixation_onset', alignment_offset=0, trials='FixTunePost')
 
+    sess.assign_learning_directions()
 
     return sess
-
-
-def parse_learning_directions(sess):
-    """ Parse through the block/trial names established above to find the main
-    learning block and direction. This then allows us to determine the other
-    directions and unlearning with respect to this etc. """
-    n_max_learn = 0
-    b_max_learn = None
-    for block in sess.blocks.keys():
-        if "Learn" in block:
-            block_len = sess.blocks[block][1] - sess.blocks[block][0]
-            if block_len > n_max_learn:
-                n_max_learn = block_len
-                b_max_learn = block
-    if b_max_learn is None:
-        # We did NOT find a learning block
-        return
-    # We found a learning block so carry on
-    pursuit_dir, learn_dir = b_max_learn.split("Learn")
-    pursuit_dir = int(pursuit_dir)
-    learn_dir = int(learn_dir)
-    anti_pursuit_dir = (pursuit_dir + 180) % 360
-    anti_learn_dir = (learn_dir + 180) % 360
-
-    # Rotation matrix so pursuit direction is at 0 degrees
-    rotate_pursuit_mat = np.array([[np.cos(np.deg2rad(0 - pursuit_dir)), -1*np.sin(np.deg2rad(0 - pursuit_dir))],
-                                   [np.sin(np.deg2rad(0 - pursuit_dir)), np.cos(np.deg2rad(0 - pursuit_dir))]])
-    # Unit vector in learing direction (round to 3 decimals to remove precision error from trig above
-    learn_vector = np.around(np.array([np.cos(np.deg2rad(learn_dir)), np.sin(np.deg2rad(learn_dir))]), 3)
-    # Transformation of learning vector with rotation matrix and mirror if needed
-    rotate_pursuit_mat = rotate_pursuit_mat @ np.array([[1., 0.], rotate_pursuit_mat @ learn_vector])
-
-
-def make_180(angle):
-    """ Helper function that, given an input angle in degrees, returns the same
-    angle on the interval [-179, 180]. """
-    # reduce the angle
-    angle = angle % 360
-    # force it to be the positive remainder, so that 0 <= angle < 360
-    angle = (angle + 360) % 360
-    # force into the minimum absolute value residue class, so that -180 < angle <= 180
-    if (angle > 180):
-        angle -= 360
-    return angle
-
-
-def get_rotation_matrix(pursuit_dir, learn_dir):
-    """ Returns the rotation matrix for transforming the pursuit_dir to angle
-    0 and learn_dir to angle 90 for 2D vectors of eye/target position.
-
-    pursuit_dir and learn_dir are assumed to be between 0 and 359 and orthogonal
-    to one another."""
-    if ( (pursuit_dir >= 360) or (pursuit_dir < 0) or
-         (learn_dir >= 360) or (learn_dir < 0) or
-         (np.abs(learn_dir - pursuit_dir) != 90) ):
-        raise RuntimeError("Pursuit direction and learning direction not valid. Pursuit dir = {0} and Learning dir = {1}.".format(pursuit_dir, learn_dir))
-
-    # Rotate clockwise for pursuit direction to be at angle 0
-    rot_angle_rad = np.deg2rad(0 - pursuit_dir)
-    # Rotation matrix so pursuit direction is at 0 degrees, rotating clockwise
-    rotate_pursuit_mat = np.array([[np.cos(rot_angle_rad), -1*np.sin(rot_angle_rad)],
-                                   [np.sin(rot_angle_rad), np.cos(rot_angle_rad)]])
-    # Round to nearest 5 decimals so that vectors stay on [0, 1], [1, 0], e.g.
-    rotate_pursuit_mat = np.around(rotate_pursuit_mat, 5)
-    mirror_xaxis = np.array([[1, 0], [0, -1]])
-    if make_180(learn_dir - pursuit_dir) == -90:
-        # Need to mirror about the x-axis
-        # Note: MUST be careful in re-using matrix variable names as @ operator
-        #       overwrites things in place, so this seems slightly long
-        rotation_matrix = mirror_xaxis @ rotate_pursuit_mat
-    else:
-        rotation_matrix = rotate_pursuit_mat
-
-    return rotation_matrix
---
+++ LearnDirTunePurk/ldp_session.py
@@ -0,0 +1,180 @@
+import numpy as np
+from SessionAnalysis.session import Session
+
+
+
+def parse_learning_directions(sess):
+    """ Parse through the block/trial names established above to find the main
+    learning block and direction. This then allows us to determine the other
+    directions and unlearning with respect to this etc. """
+    n_max_learn = 0
+    b_max_learn = None
+    for block in sess.blocks.keys():
+        if "Learn" in block:
+            block_len = sess.blocks[block][1] - sess.blocks[block][0]
+            if block_len > n_max_learn:
+                n_max_learn = block_len
+                b_max_learn = block
+    if b_max_learn is None:
+        # We did NOT find a learning block
+        return
+    # We found a learning block so carry on
+    pursuit_dir, learn_dir = b_max_learn.split("Learn")
+    pursuit_dir = int(pursuit_dir)
+    learn_dir = int(learn_dir)
+    anti_pursuit_dir = (pursuit_dir + 180) % 360
+    anti_learn_dir = (learn_dir + 180) % 360
+
+    directions = {}
+    directions['pursuit'] = pursuit_dir
+    directions['learning'] = learn_dir
+    directions['anti_pursuit'] = anti_pursuit_dir
+    directions['anti_learning'] = anti_learn_dir
+
+    return directions
+
+
+def make_180(angle):
+    """ Helper function that, given an input angle in degrees, returns the same
+    angle on the interval [-179, 180]. """
+    # reduce the angle
+    angle = angle % 360
+    # force it to be the positive remainder, so that 0 <= angle < 360
+    angle = (angle + 360) % 360
+    # force into the minimum absolute value residue class, so that -180 < angle <= 180
+    if (angle > 180):
+        angle -= 360
+
+    return angle
+
+
+class LDPSession(Session):
+
+    def __init__(self, trial_data, session_name=None, data_type=None):
+        """
+        """
+        Session.__init__(self, trial_data, session_name, data_type)
+
+    def assign_learning_directions(self):
+        """ Adds a directions dictionary indicating the learning and pursuit directions
+        and a rotation matrix property. """
+        self.directions = parse_learning_directions(self)
+        # New learning directions means new rotation matrix
+        self._set_rotation_matrix()
+
+        # Set learning and washout block alias names
+        learn_trial_name = str(self.directions['pursuit']) + "Learn" + str(self.directions['learning'])
+        # Now name the learning block and washout blocks accordingly
+        for block in self.blocks.keys():
+            if block == learn_trial_name:
+                self.blocks['Learning'] = self.blocks[block]
+                break
+        washout_trial_name = str(self.directions['pursuit']) + "Learn" + str(self.directions['anti_learning'])
+        for block in self.blocks.keys():
+            if ( (block == washout_trial_name)
+                and (self.blocks[block][0] >= self.blocks['Learning'][1]) ):
+                self.blocks['Washout'] = self.blocks[block]
+                break
+        # Recheck block order
+        self._verify_block_order()
+        return
+
+    def set_trial_types_indices(self):
+        """ Adds a "trial_types" dictionary to the session object which indicates
+        whether a trial falls into the classes given in "directions" or whether it
+        is a learning trial. """
+        instructexp = re.compile(str(self.directions['pursuit'])+"-..")
+        trial_types = {}
+        trial_types['pursuit'] = np.zeros(len(self), dtype='bool')
+        trial_types['learning'] = np.zeros(len(self), dtype='bool')
+        trial_types['anti_pursuit'] = np.zeros(len(self), dtype='bool')
+        trial_types['anti_learning'] = np.zeros(len(self), dtype='bool')
+        trial_types['instruction'] = np.zeros(len(self), dtype='bool')
+        for ind, st in enumerate(self._session_trial_data):
+            if st['name'] == self.directions['pursuit']:
+                trial_types['pursuit'][ind] = True
+            elif st['name'] == str(self.directions['pursuit']) + "RandVP":
+                trial_types['pursuit'][ind] = True
+            elif st['name'] == str(self.directions['pursuit']) + "Stab":
+                trial_types['pursuit'][ind] = True
+
+            elif st['name'] == self.directions['learning']:
+                trial_types['learning'][ind] = True
+            elif st['name'] == str(self.directions['learning']) + "RandVP":
+                trial_types['learning'][ind] = True
+            elif st['name'] == str(self.directions['learning']) + "Stab":
+                trial_types['learning'][ind] = True
+
+            elif st['name'] == self.directions['anti_pursuit']:
+                trial_types['anti_pursuit'][ind] = True
+            elif st['name'] == str(self.directions['anti_pursuit']) + "RandVP":
+                trial_types['anti_pursuit'][ind] = True
+            elif st['name'] == str(self.directions['anti_pursuit']) + "Stab":
+                trial_types['anti_pursuit'][ind] = True
+
+            elif st['name'] == self.directions['anti_learning']:
+                trial_types['anti_learning'][ind] = True
+            elif st['name'] == str(self.directions['anti_learning']) + "RandVP":
+                trial_types['anti_learning'][ind] = True
+            elif st['name'] == str(self.directions['anti_learning']) + "Stab":
+                trial_types['anti_learning'][ind] = True
+
+            elif bool(re.match(instructexp, st['name'])):
+                trial_types['instruction'][ind] = True
+        self.trial_types = trial_types
+        return
+
+    def _set_rotation_matrix(self):
+        """ Sets the rotation matrix for transforming the pursuit_dir to angle
+        0 and learn_dir to angle 90 for 2D vectors of eye/target position. Done
+        based on the directions assigned to self.directions, e.g. via
+        "assign_learning_directions". """
+        if ( (self.directions['pursuit'] >= 360) or (self.directions['pursuit'] < 0) or
+             (self.directions['learning'] >= 360) or (self.directions['learning'] < 0) or
+             (np.abs(self.directions['learning'] - self.directions['pursuit']) != 90) ):
+            raise RuntimeError("Pursuit direction and learning direction not valid. Pursuit dir = {0} and Learning dir = {1}.".format(self.directions['pursuit'], self.directions['learning']))
+
+        # Rotate clockwise for pursuit direction to be at angle 0
+        rot_angle_rad = np.deg2rad(0 - self.directions['pursuit'])
+        # Rotation matrix so pursuit direction is at 0 degrees, rotating clockwise
+        rotate_pursuit_mat = np.array([[np.cos(rot_angle_rad), -1*np.sin(rot_angle_rad)],
+                                       [np.sin(rot_angle_rad), np.cos(rot_angle_rad)]])
+        # Round to nearest 5 decimals so that vectors stay on [0, 1], [1, 0], e.g.
+        rotate_pursuit_mat = np.around(rotate_pursuit_mat, 5)
+        mirror_xaxis = np.array([[1, 0], [0, -1]])
+        if make_180(self.directions['learning'] - self.directions['pursuit']) == -90:
+            # Need to mirror about the x-axis
+            # Note: MUST be careful in re-using matrix variable names as @ operator
+            #       overwrites things in place, so this seems slightly long
+            self.rotation_matrix  = mirror_xaxis @ rotate_pursuit_mat
+        else:
+            self.rotation_matrix  = rotate_pursuit_mat
+        return
+
+    def _verify_block_order(self):
+        """ Checks that the pre and post block names in fact come before/after the
+        selected learning block. """
+        learn_start, learn_stop = self.blocks['Learning']
+        for block in self.blocks.keys():
+            if block == "FixTunePre":
+                if self.blocks[block][1] > learn_start:
+                    raise RuntimeError("{0} block ends at trial {1} which is after the learning block begins at trial {2}.".format(block, self.blocks[block][1], learn_start))
+            elif block == "FixTunePost":
+                if self.blocks[block][0] < learn_stop:
+                    raise RuntimeError("{0} block starts at trial {1} which is before the learning block ends at trial {2}.".format(block, self.blocks[block][0], learn_stop))
+            elif block == "RandVP":
+                if self.blocks[block][1] > learn_start:
+                    raise RuntimeError("{0} block ends at trial {1} which is after the learning block begins at trial {2}.".format(block, self.blocks[block][1], learn_start))
+            elif block == "StabPre":
+                if self.blocks[block][1] > learn_start:
+                    raise RuntimeError("{0} block ends at trial {1} which is after the learning block begins at trial {2}.".format(block, self.blocks[block][1], learn_start))
+            elif block == "StabPost":
+                if self.blocks[block][0] < learn_stop:
+                    raise RuntimeError("{0} block starts at trial {1} which is before the learning block ends at trial {2}.".format(block, self.blocks[block][0], learn_stop))
+            elif block == "TunePre":
+                if self.blocks[block][1] > learn_start:
+                    raise RuntimeError("{0} block ends at trial {1} which is after the learning block begins at trial {2}.".format(block, self.blocks[block][1], learn_start))
+            elif block == "TunePost":
+                if self.blocks[block][0] < learn_stop:
+                    raise RuntimeError("{0} block starts at trial {1} which is before the learning block ends at trial {2}.".format(block, self.blocks[block][0], learn_stop))
+        return
