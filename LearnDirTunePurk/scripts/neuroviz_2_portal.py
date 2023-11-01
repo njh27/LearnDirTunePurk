d7619f9dd4853c135e3a14f68bca56ee35bab329
Nate <njh27@duke.edu>
2023-11-01 15:09:49 -0400

    Corrected channel naming for portal conversion

--- LearnDirTunePurk/scripts/neuroviz_2_portal.py
+++ LearnDirTunePurk/scripts/neuroviz_2_portal.py
@@ -10,22 +10,36 @@ def neurons_2_unit_info(neurons):
                  'snr': [],
                  'template': []
                 }
+    if "filename" not in neurons[0]:
+        # Old Yoda files that I sorted from MatLab data do not have "filename" 
+        # and all recorded on SPK channel only
+        plx_chan_type = "SPK"
+    else:
+        if "Dandy" in neurons[0]['filename']:
+            # All Dandy files used WB
+            plx_chan_type = "WB"
+        elif "Yoda" in neurons[0]['filename']:
+            # All Remaining Yoda PL2 files (after 20) used SPKC only
+            plx_chan_type = "SPKC"
+        else:
+            raise ValueError(f"Could not find a Plexon channel type for {neurons[0]['filename']}")
+    
     for n in neurons:
         # If this is a PC then add its CS as a separate unit
         if n['type__'] == "NeurophysToolbox.PurkinjeCell":
             try:
-                unit_info['channel'].append(n['channel_id__'][0])
+                unit_info['channel'].append(f"{plx_chan_type}{n['channel_id__'][0]:02d}")
             except IndexError:
-                unit_info['channel'].append(n['channel_id__'])
+                unit_info['channel'].append(f"{plx_chan_type}{n['channel_id__']:02d}")
             unit_info['spiketimes'].append(n['cs_spike_indices__'] / n['sampling_rate__'])
             if 'cs_snr' in n:
                 unit_info['snr'].append(n['cs_snr'])
             if 'cs_template' in n:
                 unit_info['template'].append(n['cs_template'])
         try:
-            unit_info['channel'].append(n['channel_id__'][0])
+            unit_info['channel'].append(f"{plx_chan_type}{n['channel_id__'][0]:02d}")
         except IndexError:
-            unit_info['channel'].append(n['channel_id__'])
+            unit_info['channel'].append(f"{plx_chan_type}{n['channel_id__']:02d}")
         unit_info['spiketimes'].append(n['spike_indices__'] / n['sampling_rate__'])
         if 'snr' in n:
             unit_info['snr'].append(n['snr'])
@@ -47,5 +61,5 @@ if __name__ == "__main__":
             neurons = pickle.load(fp)
         unit_info = neurons_2_unit_info(neurons)
         portal_name = fname.replace("viz", "portal")
-        with open(os.path.join(args.portal_dir, portal_name), 'wb') as fp:
+        with open(os.path.join(args.save_dir, portal_name), 'wb') as fp:
             pickle.dump(unit_info, fp)
